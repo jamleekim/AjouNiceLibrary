6e2d826efe6437f4c742659e18eb457cf31ad3c6
jamleekim <slam67@naver.com>
2018-05-10 21:46:36 +0900

    splash추가

---
+++ app/src/main/java/com/example/slam6/ajounicelibrary/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.slam6.ajounicelibrary;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    /** 로딩 화면이 떠있는 시간(밀리초단위)  **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    /** 처음 액티비티가 생성될때 불려진다. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+
+        /* SPLASH_DISPLAY_LENGTH 뒤에 메뉴 액티비티를 실행시키고 종료한다.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
+                Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
