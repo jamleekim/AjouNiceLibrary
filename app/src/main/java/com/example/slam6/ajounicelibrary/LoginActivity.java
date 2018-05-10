b4dc7262b38f3a9b365407ce9513106abb230156
jamleekim <slam67@naver.com>
2018-05-10 21:24:19 +0900

    logo image insert.

--- app/src/main/java/com/example/slam6/ajounicelibrary/LoginActivity.java
+++ app/src/main/java/com/example/slam6/ajounicelibrary/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.slam6.ajounicelibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button b = (Button)findViewById(R.id.LoginButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( getApplicationContext(), MainActivity.class);
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
     }
 }
