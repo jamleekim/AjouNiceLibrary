1fb10c2315c89eb8ce9e7161104f445d1a9ad04c
jamleekim <slam67@naver.com>
2018-05-11 00:15:18 +0900

    app 아이콘 수정

--- app/src/main/java/com/example/slam6/ajounicelibrary/LoginActivity.java
+++ app/src/main/java/com/example/slam6/ajounicelibrary/LoginActivity.java
@@ -13,11 +13,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getWindow().setWindowAnimations(0);
-        Button b = (Button)findViewById(R.id.LoginButton);
-        b.setOnClickListener(new View.OnClickListener() {
+        Button LoginButton = (Button)findViewById(R.id.LoginButton); //로그인버튼id가져오기
+        LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent( getApplicationContext(), MainActivity.class);
+                startActivity(intent); // 메인 화면으로 넘어간다
+            }
+        });
+        Button RegisterButton = (Button)findViewById(R.id.RegisterButton); //회원가입버튼id가져오기
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( getApplicationContext(), RegisterActivity.class);
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
