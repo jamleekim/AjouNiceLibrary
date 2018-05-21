e4a6237d2c7cf267bab7b4906ca09347a458e8c6
jamleekim <slam67@naver.com>
2018-05-21 18:19:29 +0900

    qr 코드 세로모드 가능.

---
+++ app/src/main/java/com/example/slam6/ajounicelibrary/CaptureOrientationActivity.java
@@ -0,0 +1,10 @@
+package com.example.slam6.ajounicelibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.journeyapps.barcodescanner.ViewfinderView;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureOrientationActivity extends CaptureActivity {
+
+}
